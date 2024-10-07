044fbfca834dd4fd27cf77e1b6975d63497ed706
Nguyễn Thanh Sang <sangnt6604@gmail.com>
2024-10-07 09:23:06 +0700

    des-updated

--- js/destination.js
+++ js/destination.js
@@ -5,12 +5,12 @@ $(function () {
 for(let i = 1; i <= 4; i++)
     document.getElementsByClassName("place-container__right")[i-1].style.backgroundImage = `url(/images/destination/place${i}.png)`;
 function fadeIn (placeNumber) {
-    document.getElementById("background").style.display = "block";
-    document.getElementById(`place${placeNumber}`).style.display = "flex";
+    document.getElementById("background").style.scale = "1";
+    document.getElementById(`place${placeNumber}`).style.scale = "1";
 }
 function fadeOut () {
-    document.getElementById("background").style.display = "none";
+    document.getElementById("background").style.scale = "0";
     for(let i = 1; i <= 4; i++)
-        document.getElementById(`place${i}`).style.display = "none";
+        document.getElementById(`place${i}`).style.scale = "0";
 }
 
