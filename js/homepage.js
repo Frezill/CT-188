3c0e984e5e212a0df0f8dd6131bbc008d8248554
Frezill <lephatpeter@gmail.com>
2024-10-25 20:24:15 +0700

    home page

--- js/homepage.js
+++ js/homepage.js
@@ -2,3 +2,31 @@ $(function () {
     $("#header").load("../components/header.html");
     $("#footer").load("../components/footer.html");
 });
+
+const videos = document.querySelectorAll('.video-slider');
+const navBtns = document.querySelectorAll('.slider-navigation .nav-btn');
+const contents = document.querySelectorAll('.content');
+
+let currentIndex = 0;
+function updateActiveSlide(index) {
+    videos.forEach((video, idx) => {
+        video.classList.toggle('active', idx === index);
+    });
+
+    contents.forEach((content, idx) => {
+        content.classList.toggle('active', idx === index);
+    });
+
+    navBtns.forEach((btn, idx) => {
+        btn.classList.toggle('active', idx === index);
+    });
+}
+
+navBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        currentIndex = index;
+        updateActiveSlide(currentIndex);
+    });
+});
+
+updateActiveSlide(currentIndex);
