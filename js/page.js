26eae6bbbbd0685d8657a46c2b7d222405047801
Frezill <lephatpeter@gmail.com>
2024-11-04 07:25:23 +0700

    fix loading page & weather forecast

--- js/page.js
+++ js/page.js
@@ -1,9 +1,18 @@
 window.addEventListener("load", () => {
     document.querySelector("body").style.overflow = "unset";
     document.querySelector(".page-loading").classList.add("--fade");
-})
-
-$(function () {
-    $("#header").load("../components/header.html");
-    $("#footer").load("../components/footer.html");
 });
+
+(function () {
+    fetch("../components/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        });
+
+    fetch("../components/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+})();
